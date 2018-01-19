afbf52911b392a481f80afd375cb27942772a488
Michael T Smith <lionfishy@gmail.com>
2018-01-19 12:27:20 +0000

    harder problem (might take too long?)

--- jupyter/profiling.py
+++ jupyter/profiling.py
@@ -3,8 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 ip = None
 l = 1.0
-totalits = 20
-X = np.array([[0,0,0,0,0],[0,0,1,0,0],[1,0,0,0,0],[1,0,0,0,1],[1,0,0,1,0],[1,0,1,0,1],[0,1,1,0,1],[1,1,0,1,1],[0,1,1,1,1],[1,1,1,1,1]])*1.0
-Y = np.array([[-1,-1,-1,-1,-1,1,1,1,1,1]]).T*1.0
+
+#X = np.array([[0,0,0,0,0],[0,0,1,0,0],[1,0,0,0,0],[1,0,0,0,1],[1,0,0,1,0],[1,0,1,0,1],[0,1,1,0,1],[1,1,0,1,1],[0,1,1,1,1],[1,1,1,1,1]])*1.0
+
+
+N = 20
+D = 30
+totalits = 50
+X = np.random.rand(N,D)
+Y = 2.0*(np.random.rand(N,1)>0.5)-1.0 #-1s and 1s
+#Y = np.array([[-1,-1,-1,-1,-1,1,1,1,1,1]]).T*1.0
 pos_allshifts,pos_dbinfo = getshiftbounds(X,Y,l=l,totalits=totalits)
 neg_allshifts,pos_dbinfo = getshiftbounds(X,-Y,l=l,totalits=totalits)
